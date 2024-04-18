cd137a24f4210694be7a8c5621bd068ed5a77479
Vidit Jaiswal <jaiswalvidit23@gmail.com>
2024-04-18 16:02:56 +0530

    add

--- controllers/FileControllers.js
+++ controllers/FileControllers.js
@@ -25,14 +25,14 @@ console.log(request.file,'phots')
 }
 
 exports.getImage = async (request, response) => {
-    console.log(request.params.filename);
+    console.log(request.params.filename,'got it ');
     try {   
         const file = await gfs.files.findOne({ filename: request.params.filename });
         console.log(file);
         // const file1 = await gfs.photos.findOne({ filename: request.params.filename });
         console.log(file,'data');
         // consolr.log(file1,'photo');
-        const readStream = gridfsBucket.openDownloadStream(file.id);
+        const readStream = gridfsBucket.openDownloadStream(file?._id);
         readStream.pipe(response);
     } catch (error) {
         response.status(500).json({ msg: error.message });
